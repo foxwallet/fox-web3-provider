b68b500854b113128df493d84a11f48d8fd113a5
web3luhao <luhao@web3.com>
2026-05-21 16:12:02 +0800

    fix: asset type

--- src/qtum_provider.js
+++ src/qtum_provider.js
@@ -656,7 +656,7 @@ class FoxQtumProvider extends BaseProvider {
   wallet_watchAsset(payload) {
     let options = payload.params.options;
     this.postMessage("watchAsset", payload.id, {
-      type: payload.type,
+      type: payload.params.type,
       contract: options.address,
       symbol: options.symbol,
       decimals: options.decimals || 0,
